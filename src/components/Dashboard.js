b19c87c707238c46f32197befa6635db56365adf
Calinpet <92047573+Calinpet@users.noreply.github.com>
2022-09-19 20:13:44 -0400

    first commit

---
+++ src/components/Dashboard.js
@@ -0,0 +1,14 @@
+
+
+function Dashboard(){
+  return(
+    <div className="wireframe">
+      <div className="dashboard">
+         
+      </div>
+
+      </div>
+  )
+}
+
+export default Dashboard;
